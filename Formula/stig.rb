72d1ba65341e5a944bba299f3cc04340c9fe49f8
thde <thde@users.noreply.github.com>
2021-03-29 19:33:58 +0200

    init

---
+++ Formula/stig.rb
@@ -0,0 +1,95 @@
+class Stig < Formula
+    include Language::Python::Virtualenv
+  
+    desc "TUI and CLI for the BitTorrent client Transmission."
+    homepage "https://pypi.org/project/stig/"
+    url "https://files.pythonhosted.org/packages/89/68/d65356cbc6aa7035e275f4d0a57034a25bc3ba35b6025f04c8f83e6e7eee/stig-0.12.1a0.tar.gz"
+    sha256 "c984a220dbe705753cd5ac0d6948a91d051853bc753181a440e5cb489667d866"
+    license "GPL-3.0"
+  
+    depends_on "python@3.9"
+
+    resource "aiohttp" do
+        url "https://files.pythonhosted.org/packages/99/f5/90ede947a3ce2d6de1614799f5fea4e93c19b6520a59dc5d2f64123b032f/aiohttp-3.7.4.post0.tar.gz"
+        sha256 "493d3299ebe5f5a7c66b9819eacdcfbbaaf1a8e84911ddffcdc48888497afecf"
+      end
+    
+      resource "async-timeout" do
+        url "https://files.pythonhosted.org/packages/a1/78/aae1545aba6e87e23ecab8d212b58bb70e72164b67eb090b81bb17ad38e3/async-timeout-3.0.1.tar.gz"
+        sha256 "0c3c816a028d47f659d6ff5c745cb2acf1f966da1fe5c19c77a70282b25f4c5f"
+      end
+    
+      resource "attrs" do
+        url "https://files.pythonhosted.org/packages/f0/cb/80a4a274df7da7b8baf083249b0890a0579374c3d74b5ac0ee9291f912dc/attrs-20.3.0.tar.gz"
+        sha256 "832aa3cde19744e49938b91fea06d69ecb9e649c93ba974535d08ad92164f700"
+      end
+    
+      resource "blinker" do
+        url "https://files.pythonhosted.org/packages/1b/51/e2a9f3b757eb802f61dc1f2b09c8c99f6eb01cf06416c0671253536517b6/blinker-1.4.tar.gz"
+        sha256 "471aee25f3992bd325afa3772f1063dbdbbca947a041b8b89466dc00d606f8b6"
+      end
+    
+      resource "chardet" do
+        url "https://files.pythonhosted.org/packages/ee/2d/9cdc2b527e127b4c9db64b86647d567985940ac3698eeabc7ffaccb4ea61/chardet-4.0.0.tar.gz"
+        sha256 "0d6f53a15db4120f2b08c94f11e7d93d2c911ee118b6b30a04ec3ee8310179fa"
+      end
+    
+      resource "idna" do
+        url "https://files.pythonhosted.org/packages/9f/24/1444ee2c9aee531783c031072a273182109c6800320868ab87675d147a05/idna-3.1.tar.gz"
+        sha256 "c5b02147e01ea9920e6b0a3f1f7bb833612d507592c837a6c49552768f4054e1"
+      end
+    
+      resource "mock" do
+        url "https://files.pythonhosted.org/packages/e2/be/3ea39a8fd4ed3f9a25aae18a1bff2df7a610bca93c8ede7475e32d8b73a0/mock-4.0.3.tar.gz"
+        sha256 "7d3fbbde18228f4ff2f1f119a45cdffa458b4c0dee32eb4d2bb2f82554bac7bc"
+      end
+    
+      resource "multidict" do
+        url "https://files.pythonhosted.org/packages/1c/74/e8b46156f37ca56d10d895d4e8595aa2b344cff3c1fb3629ec97a8656ccb/multidict-5.1.0.tar.gz"
+        sha256 "25b4e5f22d3a37ddf3effc0710ba692cfc792c2b9edfb9c05aefe823256e84d5"
+      end
+    
+      resource "natsort" do
+        url "https://files.pythonhosted.org/packages/06/73/132e1986f7d37826e39825097e09f2c86b1339609926210ebdaec74a3b72/natsort-7.1.1.tar.gz"
+        sha256 "00c603a42365830c4722a2eb7663a25919551217ec09a243d3399fa8dd4ac403"
+      end
+    
+      resource "pyxdg" do
+        url "https://files.pythonhosted.org/packages/6f/2e/2251b5ae2f003d865beef79c8fcd517e907ed6a69f58c32403cec3eba9b2/pyxdg-0.27.tar.gz"
+        sha256 "80bd93aae5ed82435f20462ea0208fb198d8eec262e831ee06ce9ddb6b91c5a5"
+      end
+    
+      resource "stig" do
+        url "https://files.pythonhosted.org/packages/89/68/d65356cbc6aa7035e275f4d0a57034a25bc3ba35b6025f04c8f83e6e7eee/stig-0.12.1a0.tar.gz"
+        sha256 "c984a220dbe705753cd5ac0d6948a91d051853bc753181a440e5cb489667d866"
+      end
+    
+      resource "typing-extensions" do
+        url "https://files.pythonhosted.org/packages/16/06/0f7367eafb692f73158e5c5cbca1aec798cdf78be5167f6415dd4205fa32/typing_extensions-3.7.4.3.tar.gz"
+        sha256 "99d4073b617d30288f569d3f13d2bd7548c3a7e4c8de87db09a9d29bb3a4a60c"
+      end
+    
+      resource "urwid" do
+        url "https://files.pythonhosted.org/packages/94/3f/e3010f4a11c08a5690540f7ebd0b0d251cc8a456895b7e49be201f73540c/urwid-2.1.2.tar.gz"
+        sha256 "588bee9c1cb208d0906a9f73c613d2bd32c3ed3702012f51efe318a3f2127eae"
+      end
+    
+      resource "urwidtrees" do
+        url "https://files.pythonhosted.org/packages/43/e1/ca5cf122cf1121b55acb39a1fb7e9fb1283c2eb0dc75fca751eb8c16b2f9/urwidtrees-1.0.3.tar.gz"
+        sha256 "50b19c06b03a5a73e561757a26d449cfe0c08afabe5c0f3cd4435596bdddaae9"
+      end
+    
+      resource "yarl" do
+        url "https://files.pythonhosted.org/packages/97/e7/af7219a0fe240e8ef6bb555341a63c43045c21ab0392b4435e754b716fa1/yarl-1.6.3.tar.gz"
+        sha256 "8a9066529240171b68893d60dca86a763eae2139dd42f42106b03cf4b426bf10"
+      end
+  
+    def install
+      virtualenv_install_with_resources
+    end
+  
+    test do
+      system "false"
+    end
+  end
+  
